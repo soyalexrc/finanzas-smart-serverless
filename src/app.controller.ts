2a3c052a054cb552e1a4a3a762e02262cea82387
Alex Rodriguez <alexcarvajal2404@gmail.com>
2025-04-06 12:04:54 -0500

    add endpoint for asset links with JSON response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,4 +22,26 @@ export class AppController {
     // Send the JSON response
     return res.json(jsonResponse);
   }
+
+  @Get('/.well-known/assetlinks.json')
+  async getAssetLinks(@Res() res: Response) {
+    const jsonResponse = [
+      {
+        relation: ['delegate_permission/common.get_login_creds'],
+        target: {
+          namespace: 'android_app',
+          package_name: 'com.alexrc.finanzas_ok', // Example package name
+          sha256_cert_fingerprints: [
+            'F9:E0:9D:AC:92:29:62:59:57:91:E6:6A:79:41:A1:29:CD:71:A0:12:99:9B:DD:97:13:3C:31:58:E7:EE:83:2B', // Example SHA-256 fingerprint
+          ],
+        },
+      },
+    ];
+
+    // Set Content-Type to application/json
+    res.setHeader('Content-Type', 'application/json');
+
+    // Send the JSON response
+    return res.json(jsonResponse);
+  }
 }
