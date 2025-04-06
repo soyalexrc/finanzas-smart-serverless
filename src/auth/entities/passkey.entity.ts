aca7afd8a12dc0a5e6dced47847bd0ff9e1f876e
Alex Rodriguez <alexcarvajal2404@gmail.com>
2025-04-06 12:16:16 -0500

    refactor passkey entity to remove unused AuthenticatorTransportFuture import

--- src/auth/entities/passkey.entity.ts
+++ src/auth/entities/passkey.entity.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { User } from '../../user/entities/user.entity';
-import {AuthenticatorTransportFuture} from "@simplewebauthn/server";
 
 export type PasskeyDocument = HydratedDocument<Passkey>;
 
